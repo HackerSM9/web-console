e2044bc2dc26a30a4028c147caa34ad2e221cc69
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-08-24 20:57:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-let url = "https://www.instagram.com/instagram;
-$.ajax({
-    type: 'GET',
-    url: url,
-    error: function () {
-        //..
-    },
-    success: function (data) {
-        data = JSON.parse(data.split("window._sharedData = ")[1].split(";</script>")[0]).entry_data.ProfilePage[0].graphql;
-        document.write(data);
-    }
-})
+
